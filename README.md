4a83b6c24b5b211affc34bbcc481150f77815862
david broscious <dbroscious@icloud.com>
2018-02-25 16:51:42 -0500

    initial commit of project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # webflux-demo
+
+This is a simple app built on the ['Building a Reactive RESTful Web Service' Spring tutorial](https://spring.io/guides/gs/reactive-rest-service/#scratch).
---
+++ src/main/java/sdb/webfluxdemo/WebfluxDemoApplication.java
@@ -0,0 +1,15 @@
+package sdb.webfluxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxDemoApplication.class, args);
+
+		GreetingWebClient gwc = new GreetingWebClient();
+		System.out.println(gwc.getResult());
+	}
+}
