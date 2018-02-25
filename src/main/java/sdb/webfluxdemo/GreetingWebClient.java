1b50ca6a7d46af9480cb9193125ffc0e30467e6b
david broscious <dbroscious@icloud.com>
2018-02-25 17:34:28 -0500

    more experiements

--- src/main/java/sdb/webfluxdemo/GreetingWebClient.java
+++ src/main/java/sdb/webfluxdemo/GreetingWebClient.java
@@ -14,7 +14,16 @@ public class GreetingWebClient {
             .accept(MediaType.TEXT_PLAIN)
             .exchange();
 
+    private Mono<ClientResponse> resultbob = client.get()
+            .uri("/bob")
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange();
+
     public String getResult() {
         return ">> result = " + result.flatMap(res -> res.bodyToMono(String.class)).block();
+    }
+
+    public String getResultBob() {
+        return ">> resultBob = " + resultbob.flatMap(res -> res.bodyToMono(String.class)).block();
     }
 }
